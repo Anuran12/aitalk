1c81ea9ae3dd4a6c8a7768ebf2f7b694d9297834
Anuran12 <anuranarts@gmail.com>
2024-11-15 09:06:57 +0530

    topbar

---
+++ components/Topbar.tsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+import React from "react";
+import Logo from "@/public/Logo.png";
+import Link from "next/link";
+
+export default function Topbar() {
+  return (
+    <div className=" w-full grid grid-cols-3 items-center justify-center px-[8vw]">
+      <div className="w-full"></div>
+      <div className="w-full flex justify-center items-center">
+        <Image src={Logo} alt="" />
+      </div>
+      <div className="w-full flex gap-5 justify-end items-center">
+        <Link
+          href={"/"}
+          className="border-2 border-[#2E3036] px-5 py-2.5 rounded-full"
+        >
+          About Us
+        </Link>
+        <Link
+          href={"/"}
+          className="border-2 border-[#2E3036] px-5 py-2.5 rounded-full"
+        >
+          Contact US
+        </Link>
+      </div>
+    </div>
+  );
+}

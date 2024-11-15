7e5ca58990e27653a840d3f8611688ec1b5f900d
Anuran12 <anuranarts@gmail.com>
2024-11-15 09:58:55 +0530

    home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import Ask from "@/components/Ask";
 import Topbar from "@/components/Topbar";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-items-center min-h-screen p-8">
+    <div className="flex flex-col items-center justify-items-center h-screen p-8">
       <Topbar />
+      <Ask />
     </div>
   );
 }

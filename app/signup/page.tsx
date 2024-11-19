bb20a7569a3f88c40f34f4f53b4ac13cef47df8c
Anuran12 <anuranarts@gmail.com>
2024-11-19 08:59:41 +0530

    fix

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,11 +1,23 @@
-import React from "react";
+"use client";
+import React, { useEffect } from "react";
 import SignupImg from "@/public/signup.png";
 import Image from "next/image";
 import Link from "next/link";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import { signIn, useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 export default function SignupPage() {
+  const { data: session } = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (session) {
+      // Redirect to the homepage if the user is logged in
+      router.push("/");
+    }
+  }, [session, router]);
   return (
     <div className="flex flex-col gap-3 items-center justify-between min-h-screen p-8">
       <Header />
@@ -62,7 +74,10 @@ export default function SignupPage() {
         </svg>
       </div>
       <div className="flex gap-3">
-        <button className="bg-[#393A40] rounded-full aspect-square w-[10vw] md:w-[3.5vw] flex justify-center items-center">
+        <button
+          onClick={() => signIn("google")}
+          className="bg-[#393A40] rounded-full aspect-square w-[10vw] md:w-[3.5vw] flex justify-center items-center"
+        >
           <svg
             width="25"
             viewBox="0 0 32 33"
@@ -103,18 +118,18 @@ export default function SignupPage() {
             </defs>
           </svg>
         </button>
-        <button className="bg-[#393A40] rounded-full aspect-square w-[10vw] md:w-[3.5vw] flex justify-center items-center">
+        <button
+          onClick={() => signIn("github")}
+          className="bg-[#393A40] rounded-full aspect-square w-[10vw] md:w-[3.5vw] flex justify-center items-center"
+        >
           <svg
-            width="14"
-            height="25"
-            viewBox="0 0 14 25"
-            fill="none"
             xmlns="http://www.w3.org/2000/svg"
+            x="0px"
+            y="0px"
+            width="40"
+            viewBox="0 0 30 30"
           >
-            <path
-              d="M8.86255 24.496V13.568H12.5492L13.0972 9.28934H8.86255V6.564C8.86255 5.32933 9.20655 4.484 10.9785 4.484H13.2239V0.669335C12.1315 0.551329 11.0333 0.494795 9.93455 0.500001C6.67588 0.500001 4.43855 2.48933 4.43855 6.14133V9.28134H0.775879V13.56H4.44655V24.496H8.86255Z"
-              fill="#1877F2"
-            />
+            <path d="M15,3C8.373,3,3,8.373,3,15c0,5.623,3.872,10.328,9.092,11.63C12.036,26.468,12,26.28,12,26.047v-2.051 c-0.487,0-1.303,0-1.508,0c-0.821,0-1.551-0.353-1.905-1.009c-0.393-0.729-0.461-1.844-1.435-2.526 c-0.289-0.227-0.069-0.486,0.264-0.451c0.615,0.174,1.125,0.596,1.605,1.222c0.478,0.627,0.703,0.769,1.596,0.769 c0.433,0,1.081-0.025,1.691-0.121c0.328-0.833,0.895-1.6,1.588-1.962c-3.996-0.411-5.903-2.399-5.903-5.098 c0-1.162,0.495-2.286,1.336-3.233C9.053,10.647,8.706,8.73,9.435,8c1.798,0,2.885,1.166,3.146,1.481C13.477,9.174,14.461,9,15.495,9 c1.036,0,2.024,0.174,2.922,0.483C18.675,9.17,19.763,8,21.565,8c0.732,0.731,0.381,2.656,0.102,3.594 c0.836,0.945,1.328,2.066,1.328,3.226c0,2.697-1.904,4.684-5.894,5.097C18.199,20.49,19,22.1,19,23.313v2.734 c0,0.104-0.023,0.179-0.035,0.268C23.641,24.676,27,20.236,27,15C27,8.373,21.627,3,15,3z"></path>
           </svg>
         </button>
       </div>
